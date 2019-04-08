69a056ae0593807c5ebb9c7c78cf6ca5cae4c763
Brun <gabriel.brun@accenture.com>
2019-04-08 02:00:10 +0200

    get create update profile

--- models/Profile.js
+++ models/Profile.js
@@ -3,118 +3,118 @@ const Schema = mongoose.Schema;
 
 // Create Schema
 const ProfileSchema = new Schema({
-    user: {
-        type: Schema.Types.ObjectId, // associated the user by his id
-        ref: 'users', // collection users
-    },
-    handle: {
+  user: {
+    type: Schema.Types.ObjectId, // associated the user by his id // type: mongoose.Types.ObjectId fonctionne aussi
+    ref: "users" // collection users
+  },
+  handle: {
+    type: String,
+    required: true,
+    max: 40
+  },
+  company: {
+    type: String
+  },
+  website: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  skills: {
+    type: [String],
+    required: true
+  },
+  bio: {
+    type: String
+  },
+  githubusername: {
+    type: String
+  },
+  experience: [
+    {
+      title: {
         type: String,
-        required: true,
-        max: 40
-    },
-    company: {
-        type: String
-    },
-    website: {
-        type: String
-    },
-    location: {
-        type: String
-    },
-    status: {
+        require: true
+      },
+      company: {
         type: String,
-        required: true
-    },
-    skills: {
-        type: [String],
-        required: true
-    },
-    bio: {
-        type: String,
-    },
-    githubusername: {
+        require: true
+      },
+      location: {
         type: String
-    },
-    experience: [
-        {
-            title: {
-                type: String,
-                require: true
-            },
-            company: {
-                type: String,
-                require: true
-            },
-            location: {
-                type: String,
-            },
-            from: {
-                type: Date,
-                require: true
-            },
-            to: {
-                type: Date,
-            },
-            current: {
-                type: Boolean,
-                default: false
-            },
-            description: {
-                type: String,
-            }
-        }
-    ],
-    education: [
-        {
-            school: {
-                type: String,
-                require: true
-            },
-            degree: {
-                type: String,
-                require: true
-            },
-            fieldofstudy: {
-                type: String,
-                require: true
-            },
-            from: {
-                type: Date,
-                require: true
-            },
-            to: {
-                type: Date,
-            },
-            current: {
-                type: Boolean,
-                default: false
-            },
-            description: {
-                type: String,
-            }
-        }
-    ],
-    social: {
-        youtube: {
-            type: String
-        },
-        twitter: {
-            type: String
-        },
-        facebook: {
-            type: String
-        },
-        linkedin: {
-            type: String
-        },
-        instagram: {
-            type: String
-        }
-    },
-    date: {
+      },
+      from: {
         type: Date,
-        default: Date.now
+        require: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
     }
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+        require: true
+      },
+      degree: {
+        type: String,
+        require: true
+      },
+      fieldofstudy: {
+        type: String,
+        require: true
+      },
+      from: {
+        type: Date,
+        require: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  social: {
+    youtube: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    },
+    instagram: {
+      type: String
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-module.exports = Profile = mongoose.model('profile', ProfileSchema);
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const passport = require("passport");
 
+// Load validation
+const validateProfileInput = require("../../validation/profile");
+
 // Load Profile Model
 const Profile = require("../../models/Profile");
 // Load User Profile
@@ -22,6 +25,7 @@ router.get(
   (req, res) => {
     const errors = {};
     Profile.findOne({ user: req.user.id })
+      .populate("user", ["name", "avatar"]) // get name and avatar grace à Types.ObjectId
       .then(profile => {
         if (!profile) {
           errors.noprofile = "There is no profile for this user";
@@ -39,9 +43,17 @@ router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    const { errors, isValid } = validateProfileInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
+
     // Get fields
     const profileFields = {};
-    profileFields.user = req.body.id;
+    profileFields.user = req.user.id;
     if (req.body.handle) profileFields.handle = req.body.handle;
     if (req.body.company) profileFields.company = req.body.company;
     if (req.body.website) profileFields.website = req.body.website;
@@ -74,14 +86,19 @@ router.post(
         // Create
 
         // Check if handle exists
-        Profile.findOne({ handle: profileFields.handle }).then(profile => {
-          if (profile) {
-            errors.handle = "That handle already exists";
-            res.status(400).json(errors);
-          }
-          // Save Profile
-          new Profile(profileFields).save().then(profile => res.json(profile));
-        });
+        Profile.findOne({ handle: profileFields.handle })
+          .then(profile => {
+            if (profile) {
+              errors.handle = "That handle already exists";
+              return res.status(400).json(errors);
+            }
+            // Save Profile
+            new Profile(profileFields)
+              .save()
+              .then(profile => res.json(profile))
+              .catch(err => console.log("err1" + err));
+          })
+          .catch(err => console.log("err2" + err));
       }
     });
   }
