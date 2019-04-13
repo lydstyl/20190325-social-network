6432931fa59da89aa1c513540eb69e249b8b058b
Brun <gabriel.brun@accenture.com>
2019-04-14 00:18:15 +0200

    45. Logout & Conditional Navbar Links

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -51,3 +51,13 @@ export const setCurrentUser = decoded => {
     payload: decoded
   };
 };
+
+// Log user out
+export const logoutUser = () => dispatch => {
+  // Remove token from ls
+  localStorage.removeItem("jwtToken");
+  // Remove auth header
+  setAuthToken(false);
+  // Set current user to {} wich will set isAutheticated to false
+  dispatch(setCurrentUser({}));
+};
