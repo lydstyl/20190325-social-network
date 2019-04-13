d29306164d9c71b1fa7b3116e961b9e435d7ac3c
Brun <gabriel.brun@accenture.com>
2019-04-13 13:28:35 +0200

    42. Registration & The Error Reducer

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,9 +1,26 @@
-import { TEST_DISPATCH } from "./types";
+// import { TEST_DISPATCH } from "./types";
+import { GET_ERRORS } from "./types";
+import axios from "axios";
 
 // Register User
-export const registerUser = userData => {
-  return {
-    type: TEST_DISPATCH,
-    payload: userData
-  };
+
+// export const registerUser = userData => {
+//   return {
+//     type: TEST_DISPATCH,
+//     payload: userData
+//   };
+// };
+
+export const registerUser = (userData, history) => dispatch => {
+  // plus simple qu'une fonction dans une fonction
+  axios
+    .post("/api/users/register", userData)
+    .then(res => history.push("/login"))
+    .catch(err =>
+      dispatch({
+        // on appelle dispatch au lieu de return car ajax async
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
 };
