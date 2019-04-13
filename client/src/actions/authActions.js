c973c3033321ab316cc8ecbc27f62607433ad688
Brun <gabriel.brun@accenture.com>
2019-04-13 14:45:00 +0200

    43. Redux Login Action & Set Current User

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,16 +1,11 @@
 // import { TEST_DISPATCH } from "./types";
-import { GET_ERRORS } from "./types";
 import axios from "axios";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 
 // Register User
-
-// export const registerUser = userData => {
-//   return {
-//     type: TEST_DISPATCH,
-//     payload: userData
-//   };
-// };
-
 export const registerUser = (userData, history) => dispatch => {
   // plus simple qu'une fonction dans une fonction
   axios
@@ -23,4 +18,36 @@ export const registerUser = (userData, history) => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+// Login User
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      // Save to localStorage
+      const { token } = res.data;
+      localStorage.setItem("jwtToken", token);
+      // Set token to Auth header
+      setAuthToken(token);
+      // Decode token to get user data
+      const decoded = jwt_decode(token);
+      // Set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        // on appelle dispatch au lieu de return car ajax async
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Set logged in user
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
 };
